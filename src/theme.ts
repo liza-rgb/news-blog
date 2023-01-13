e2a3c9ccd6da14ce4b743dfc04944ab5d5ccb7d5
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2023-01-13 02:20:08 +0100

    Add default text color and font weight

--- src/theme.ts
+++ src/theme.ts
@@ -3,5 +3,9 @@ import { createTheme } from '@mui/material/styles';
 export const theme = createTheme({
   typography: {
     fontFamily: ['Montserrat', 'Roboto', 'sans-serif'].join(','),
+    allVariants: {
+      color: '#363636',
+      fontWeight: 400,
+    },
   },
 });
