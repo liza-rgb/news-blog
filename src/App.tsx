f9eab48f22df150d5c8ada9c90a18e72b5b4dda8
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2023-01-14 19:19:11 +0100

    Add CSS Baseline to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { ThemeProvider } from '@mui/system';
+import { CssBaseline } from '@mui/material';
 import { theme } from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className='App'>App</div>
+      <CssBaseline />
+      <div className='App'></div>
     </ThemeProvider>
   );
 }
